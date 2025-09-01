a17c6c6b9763fb74c36950a9dca5253e22e66be5
Akash Kumar <akashkumar914a@gmail.com>
2025-09-01 13:23:04 +0530

    faq added

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
   hamburger.classList.toggle("active");
 });
+
+document.querySelectorAll('.faq-question').forEach(q => {
+  q.addEventListener('click', () => {
+    const parent = q.parentElement;
+    parent.classList.toggle('active');
+  });
+});
